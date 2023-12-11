88d090623e9f423274b4a5252ca028e56042f037
luke zhao <401613042@qq.com>
2023-12-11 21:46:00 +0800

    初始化项目

---
+++ src/main/java/com/luke/luketools/LuketoolsApplication.java
@@ -0,0 +1,13 @@
+package com.luke.luketools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuketoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuketoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luke/luketools/LuketoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luke.luketools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuketoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
